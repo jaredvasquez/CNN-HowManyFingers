ad5125cbc15e4e3c46c0ed575a8ecde824ea0084
Jared Vasquez <jared.vasquez@cern.ch>
2017-07-26 22:41:18 -0400

    updated

--- application.py
+++ application.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+''' Written by jared.vasquez@yale.edu '''
+
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,15 +8,16 @@ import copy
 import cv2
 import os
 
+
 dataColor = (0,255,0)
-takingData = False
 font = cv2.FONT_HERSHEY_SIMPLEX
 fx, fy, fh = 10, 50, 45
+takingData = 0
 className = 'NONE'
 count = 0
 showMask = 0
 
-classes = 'OPEN CLOSED'.split()
+
 classes = 'NONE ONE TWO THREE FOUR FIVE'.split()
 
 
@@ -27,7 +31,6 @@ def initClass(name):
 def binaryMask(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(img, (7,7), 3)
-    #img = cv2.GaussianBlur(img, (5,5), 2)
     img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
     ret, new = cv2.threshold(img, 25, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     return new
@@ -39,23 +42,18 @@ def main():
     global className, count
     global showMask
 
-    #model = load_model('model.h5')
     model = load_model('model_6cat.h5')
 
     x0, y0, width = 200, 220, 300
-    #x0, y0, width = 800, 200, 300
 
     cam = cv2.VideoCapture(0)
     cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
-    #cv2.namedWindow('ROI', cv2.WINDOW_NORMAL)
-    #plt.ion()
 
     while True:
         # Get camera frame
         ret, frame = cam.read()
         frame = cv2.flip(frame, 1) # mirror
         window = copy.deepcopy(frame)
-        #window = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.rectangle(window, (x0,y0), (x0+width-1,y0+width-1), dataColor, 12)
 
         # draw text
@@ -75,6 +73,7 @@ def main():
         if showMask:
             window[y0:y0+width,x0:x0+width] = cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR)
 
+        # take data or apply predictions on ROI
         if takingData:
              cv2.imwrite('images/{0}/{0}_{1}.png'.format(className, count), roi)
              count += 1
@@ -82,15 +81,10 @@ def main():
             img = np.float32(roi)/255.
             img = np.expand_dims(img, axis=0)
             img = np.expand_dims(img, axis=-1)
-            #pred = classes[ int(np.round(model.predict(img)[0][0])) ]
             pred = classes[np.argmax(model.predict(img)[0])]
             cv2.putText(window, 'Prediction: %s' % (pred), (fx,fy+2*fh), font, 1.0, (245,210,65), 2, 1)
 
-        #plt.imshow(roi, cmap='gray')
-        #plt.imshow(window)
-        #plt.pause(1e-05)
-        #cv2.imshow('frame', roi)
-        #cv2.imshow('ROI', roi)
+        # show the window
         cv2.imshow('Original', window)
 
         # Keyboard inputs
@@ -117,8 +111,6 @@ def main():
         elif key == ord('5'):  initClass('FIVE')
 
         # adjust the size of window
-        elif key == ord('z'): blur -= 1 #max((blur-1), 0)
-        elif key == ord('a'): blur += 1
         #elif key == ord('z'):
         #    width = width - 5
         #elif key == ord('a'):
